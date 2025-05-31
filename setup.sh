593f545975543b20cb33398f03da157502613bb6
mRn0b0dy <114957011+mRn0b0dye@users.noreply.github.com>
2025-05-31 19:33:04 +0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt install git gedit tor sublist3r assetfinder findomain python3-pip
+go install -v github.com/tomnomnom/gf@latest
+go install -v github.com/PentestPad/subzy@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install github.com/hahwul/dalfox/v2@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+go install github.com/003random/getJS@latest
+go install -v github.com/hakluke/haktrails@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/0xsha/GoLinkFinder@latest
+go install github.com/Brosck/mantra@latest
+go install -v github.com/projectdiscovery/urlfinder/cmd/urlfinder@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/haccer/subjack@latest
